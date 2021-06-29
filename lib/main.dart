10d9786f7c2630a35703ba7e03af631291b961b3
Sumit Kumar <SumitKumar1307>
2021-06-29 21:16:41 +0530

    made some small changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 void main(List<String> args) => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -26,73 +26,94 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: layout(),
-    );
+        child: layout(), backgroundColor: Color(0xFFE0DFFF));
   }
 
   Widget layout() {
     Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Container(
-        padding: EdgeInsets.only(
-          left: 10, top: 25
-        ),
+        padding: EdgeInsets.only(left: 10, top: 25),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Container(
-                    height: 180,
-                    width: 290,
-                    decoration: BoxDecoration(
-                      color: Color(0xFFE0DFFF),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(40),
-                      ),
+              Row(children: <Widget>[
+                Container(
+                  height: 180,
+                  width: 260,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(40),
                     ),
                   ),
-                  SizedBox(width: 10), // Big Block
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xFFE0DFFF),
-                          borderRadius: BorderRadius.all(Radius.circular(14))
-                        ),
-                        height: 75,
-                        width: 75,
-                      ), // lock
-                      SizedBox(height: 15),
-                      Container(
-                          decoration: BoxDecoration(
-                          color: Color(0xFFE0DFFF),
-                          borderRadius: BorderRadius.all(Radius.circular(14))
-                        ),
-                        height: 75,
-                        width: 75,
-                      ),
-                    ]
+                  padding: EdgeInsets.only(
+                    left: 40,
+                    right: 40,
                   ),
-                ]
-              ),
-              SizedBox(height: 20),
-                Column(
-                  children: <Widget>[
-                    for (int i=0; i<10; i++)
-                      Container(
-                        margin: EdgeInsets.only(bottom: 15),
-                        width: size.width * 0.9,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFE0DFFF),
-                          borderRadius: BorderRadius.all(Radius.circular(22))
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Your\nVault",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 30,
+                          color: CupertinoColors.activeBlue,
                         ),
-                      )
-                  ],
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            left: BorderSide(color: CupertinoColors.systemGrey),
+                          ),
+                        ),
+                        height: 60,
+                      ),
+                      Icon(Icons.security_rounded)
+                    ],
+                  ),
                 ),
-            
+                SizedBox(width: 10), // Big Block
+                Column(children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(14))),
+                    height: 75,
+                    width: 75,
+                    child: Icon(Icons.lock),
+                  ), // lock
+                  SizedBox(height: 15),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(14))),
+                    height: 75,
+                    width: 75,
+                    child: Icon(Icons.add),
+                  ),
+                ]),
+              ]),
+              SizedBox(height: 20),
+              Column(
+                children: <Widget>[
+                  for (int i = 0; i < 10; i++)
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      width: size.width * 0.9,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(22),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
             ],
           ),
         ),
